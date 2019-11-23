3995089f9c938a74e857dbafa0ca9df4750666ef
Ernest <iattempt.net@gmail.com>
2019-11-23 13:52:18 +0800

    v1.0.1
    Squashed commit of the following:
    
    commit 38d8cd0fb87350e31994ba9846a21bdff404ec86
    Author: Ernest <iattempt.net@gmail.com>
    Date:   Sat Nov 23 13:40:19 2019 +0800
    
        Pack.
    
    commit 11f4fbee15696bd1f91d8e7ededcda802b9866bd
    Author: Ernest <iattempt.net@gmail.com>
    Date:   Sat Nov 23 13:19:24 2019 +0800
    
        Force lint.
    
    commit 20e7e8b6b15092a51f51f2d4fef5334fd4cdd2d9
    Author: Ernest <iattempt.net@gmail.com>
    Date:   Sat Nov 23 12:52:41 2019 +0800
    
        Allow chaining.
    
    commit b763d46680e7e2049ed85561a233c8de8a3951e8
    Author: Ernest <iattempt.net@gmail.com>
    Date:   Sat Nov 23 02:49:42 2019 +0800
    
        Simplify.

--- src/index.js
+++ src/index.js
@@ -1,40 +1,41 @@
 const genDefaultOptions = () => ({
   regex: /\$s\(([a-zA-Z_\$]+),([^,]+),([^;]*)\);/,
   maxReplacementCount: 1000,
-})
+});
 
-module.exports = {
+export default {
   type: 'postProcessor',
   name: 'select',
   options: genDefaultOptions(),
 
-  updateOptions: function(options) {
-    this.options = {...this.options, ...options}
+  updateOptions(options) {
+    this.options = {...this.options, ...options};
+    return this;
   },
 
-  process: function process(value, _, options, translator) {
+  process(value, _, options, translator) {
     if (Object.prototype.toString.call(options.postProcess) === '[object String]') {
-      delete options.postProcess
+      delete options.postProcess;
     } else {
-      options.postProcess.splice(options.postProcess.find(name => name === this.name), 1)
+      options.postProcess.splice(options.postProcess.find((name) => name === this.name), 1);
     }
 
-    let count = 1
+    let count = 1;
     while (count++ < this.options.maxReplacementCount) {
-      const result = this.options.regex.exec(value)
-      if (result === null) break
+      const result = this.options.regex.exec(value);
+      if (result === null) break;
 
-      const [matchedString, key, expect, string] = result
-      const beforePart = value.slice(0, result.index)
-      const endPart = value.slice(result.index + matchedString.length)
+      const [matchedString, key, expect, string] = result;
+      const beforePart = value.slice(0, result.index);
+      const endPart = value.slice(result.index + matchedString.length);
 
       if (options[key] === JSON.parse(expect)) {
-        value = beforePart + string + endPart
+        value = beforePart + string + endPart;
       } else {
-        value = beforePart + endPart
+        value = beforePart + endPart;
       }
     }
 
-    return translator.translate(value, options)
-  }
-}
+    return translator.translate(value, options);
+  },
+};
