8ae1098700c9b94811f3f36d15931fa9320eed30
Ernest <iattempt.net@gmail.com>
2019-11-24 15:22:58 +0800

    v1.0.2
    Squashed commit of the following:
    
    commit 6c84b93af9dbff243ed1cb0e522420bef2457008
    Author: Ernest <iattempt.net@gmail.com>
    Date:   Sun Nov 24 15:17:58 2019 +0800
    
        Fix error.
    
    commit d681f244000a7616939a63a023ba8a33c637a7c2
    Author: Ernest <iattempt.net@gmail.com>
    Date:   Sun Nov 24 13:12:26 2019 +0800
    
        Change dist.
    
    commit 8ab2a9a1216070ae92846fb750a3b56285de84a1
    Merge: 38d8cd0 3995089
    Author: Ernest <iattempt.net@gmail.com>
    Date:   Sun Nov 24 15:21:54 2019 +0800
    
        Merge branch 'master' into develop
    
    commit 38d8cd0fb87350e31994ba9846a21bdff404ec86
    Author: Ernest <iattempt.net@gmail.com>
    Date:   Sat Nov 23 13:40:19 2019 +0800
    
        Pack.
    
    commit 11f4fbee15696bd1f91d8e7ededcda802b9866bd
    Author: Ernest <iattempt.net@gmail.com>
    Date:   Sat Nov 23 13:19:24 2019 +0800
    
        Force lint.
    
    commit 20e7e8b6b15092a51f51f2d4fef5334fd4cdd2d9
    Author: Ernest <iattempt.net@gmail.com>
    Date:   Sat Nov 23 12:52:41 2019 +0800
    
        Allow chaining.
    
    commit b763d46680e7e2049ed85561a233c8de8a3951e8
    Author: Ernest <iattempt.net@gmail.com>
    Date:   Sat Nov 23 02:49:42 2019 +0800
    
        Simplify.

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 const genDefaultOptions = () => ({
-  regex: /\$s\(([a-zA-Z_\$]+),([^,]+),([^;]*)\);/,
+  regex: /\$s\(([a-zA-Z0-9_\$]+),([^,]+),([^(;\))]*)\);/,
   maxReplacementCount: 1000,
 });
 
